259d20299f35891679fbfa8d76f24665243fa5f8
Brian710 <mmm850710@gmail.com>
2020-11-19 11:55:48 +0800

    MultiLanguageImageAdd_1119

---
+++ IdensolApp/Assets/ChangeImage.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ChangeImage : MonoBehaviour
+{
+    [SerializeField] private Image OrignalImage;
+    [SerializeField] private Sprite OrignalSprite;
+
+    [SerializeField] private Sprite TargetSprite_TC;
+    [SerializeField] private Sprite TargetSprite_SC;
+    [SerializeField] private Sprite TargetSprite_EN;
+    public void Start()
+    {
+        //OrignalImage.sprite = OrignalSprite;
+    }
+    public void ChangeTargetImage()
+    {
+        OrignalSprite = OrignalImage.sprite;
+        ChangeSpriteLanguage(LanguageManager.Instance.language);
+    }
+    private void ChangeSpriteLanguage(Language language)
+    {
+        switch (language)
+        {
+            case Language.TraditionalChinese:
+                OrignalImage.sprite = TargetSprite_TC;
+                break;
+            case Language.SimplifiedChinese:
+                OrignalImage.sprite = TargetSprite_SC;
+                break;
+            case Language.English:
+                OrignalImage.sprite = TargetSprite_EN;
+                break;
+        }
+    }
+}
