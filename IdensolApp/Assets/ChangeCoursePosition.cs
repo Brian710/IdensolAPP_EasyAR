1c8e1e72356ab2132018c8d9f74b02cf341b4b73
Brian710 <mmm850710@gmail.com>
2020-12-03 13:57:22 +0800

    UpdateChangeCoursePosition

--- IdensolApp/Assets/ChangeCoursePosition.cs
+++ IdensolApp/Assets/ChangeCoursePosition.cs
@@ -4,9 +4,7 @@ using UnityEngine;
 
 public class ChangeCoursePosition : MonoBehaviour
 {
-    public float Position_X;
-    public float Position_Y;
-    public float Position_Z;
+    public Vector3 Position;
 
     private Language Language;
     void Start()
@@ -18,7 +16,7 @@ public class ChangeCoursePosition : MonoBehaviour
     {
         if(language == Language.English)
         {
-            GetComponent<Transform>().position = new Vector3(Position_X, Position_Y, Position_Z);
+            GetComponent<Transform>().position = Position;
         }
     }
 }
