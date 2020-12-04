b16061edd4a7608742a5a25f3624c1c29910963d
Brian710 <mmm850710@gmail.com>
2020-12-04 13:37:11 +0800

    AddChangeFAQAnimator

---
+++ IdensolApp/Assets/ChangeFAQAnimator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Animations;
+using UnityEngine;
+
+public class ChangeFAQAnimator : MonoBehaviour
+{
+    public Animator Animator;
+    public AnimatorController AnimatorController;
+
+    private Language Language;
+   
+    void Start()
+    {
+        Language = LanguageManager.Instance.language;
+        Set(Language);
+    }
+
+    private void Set(Language language)
+    {
+        if(AnimatorController != null)
+        {
+            if (language == Language.English)
+            {
+                Animator.runtimeAnimatorController = AnimatorController as RuntimeAnimatorController;
+            }
+        }
+    }
+}
